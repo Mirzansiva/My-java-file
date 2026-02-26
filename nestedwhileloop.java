c6aa22a262143350c29d722adb93b7d441b167bd
Mirzan siva <mirzansiva2008@gmail.com>
2026-02-26 10:12:57 +0530

    basic

---
+++ nestedwhileloop.java
@@ -0,0 +1,15 @@
+class nestedwhileloop{
+	public static void main (String args[]){
+		
+		int x=1;
+		while (x<=5){
+			int y=1;
+			while (y<=3){
+				System.out.println(y);
+				y++;
+			}
+			System.out.println(y);
+			x++;
+		}
+	}
+}
